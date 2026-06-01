ca357584acf875d6b1a0a22a9855384ed8525964
viddiai <stefan@peakmarketing.io>
2026-06-01 16:50:07 +0200

    Add LinkedIn Insight Tag (partner_id 1789420)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,6 +88,38 @@ export default function RootLayout({
           src="https://assets.calendly.com/assets/external/widget.js"
           strategy="lazyOnload"
         />
+        <Script
+          id="linkedin-insight-partner-id"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `_linkedin_partner_id = "1789420";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);`,
+          }}
+        />
+        <Script
+          id="linkedin-insight-loader"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})(window.lintrk);`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=1789420&fmt=gif"
+          />
+        </noscript>
         <StructuredData />
         <Navbar />
         <main>{children}</main>
